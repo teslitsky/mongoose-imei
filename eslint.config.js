d672e91381db6ac70a52cf72737ae1319d196d05
Dmitry Teslitsky <dmitry.teslitsky@gen.tech>
2024-10-01 15:57:26 +0300

    Update ESLint rules
    - bump ESLint to v9
    - remove eslint-config-airbnb-base
    - remove eslint-plugin-import
    - move Prettier and ESLint configs from package.json

---
+++ eslint.config.js
@@ -0,0 +1,24 @@
+const pluginJs = require('@eslint/js');
+const globals = require('globals');
+const eslintPluginPrettierRecommended = require('eslint-plugin-prettier/recommended');
+
+module.exports = [
+  pluginJs.configs.recommended,
+  eslintPluginPrettierRecommended,
+  {
+    languageOptions: { globals: globals.node },
+  },
+  {
+    files: ['**/*.js'],
+    languageOptions: { sourceType: 'commonjs' },
+  },
+  {
+    files: ['**/*.spec.js'],
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+      },
+    },
+  },
+];
