c56c0799fd6f20d2a51a9a1c60f2af1a5ab95e8d
Dmitry Teslitsky <dmitry.teslitsky@gen.tech>
2025-10-04 13:47:30 +0300

    Migrate to native node test runner

--- eslint.config.js
+++ eslint.config.js
@@ -17,7 +17,6 @@ module.exports = [
     languageOptions: {
       globals: {
         ...globals.node,
-        ...globals.jest,
       },
     },
   },
--- tests/test.spec.js
+++ tests/test.spec.js
@@ -1,16 +1,16 @@
+const assert = require('node:assert');
+const { after, before, describe, it } = require('node:test');
 const { MongoMemoryServer } = require('mongodb-memory-server');
 const mongoose = require('mongoose');
 const ImeiModule = require('../index');
 
 const { Schema } = mongoose;
 
-jest.setTimeout(30000);
-
 let mongod;
 let SampleModel;
 
 describe('Mongoose IMEI', () => {
-  beforeAll(async () => {
+  before(async () => {
     mongod = await MongoMemoryServer.create();
     const uri = mongod.getUri();
 
@@ -24,28 +24,32 @@ describe('Mongoose IMEI', () => {
   });
 
   it('should exists loadType() function', () => {
-    expect(typeof ImeiModule.loadType).toEqual('function');
+    assert.equal(typeof ImeiModule.loadType, 'function');
   });
 
   it('should now has an "Imei" type in mongoose.Schema.Types', () => {
-    expect(Object.prototype.hasOwnProperty.call(Schema.Types, 'Imei')).toEqual(
+    assert.equal(
+      Object.prototype.hasOwnProperty.call(Schema.Types, 'Imei'),
       true,
     );
   });
 
   it('should throws an error when given invalid IMEI', async () => {
     const sampleModel = new SampleModel({ imei: '123' });
-    await expect(sampleModel.save()).rejects.toThrow('IMEI is invalid');
+    await assert.rejects(sampleModel.save(), {
+      name: 'ValidationError',
+      message: /IMEI is invalid/,
+    });
   });
 
   it('should stores the proper IMEI value without any validation errors', async () => {
     const imei = '351680077319519';
     const sampleModel = new SampleModel({ imei });
     const result = await sampleModel.save();
-    expect(result.imei).toEqual(imei);
+    assert.equal(result.imei, imei);
   });
 
-  afterAll(async () => {
+  after(async () => {
     await mongoose.disconnect();
     await mongod.stop();
   });
